940eaf71a25e28ecbb437c50dc8048e500cfc4a0
wdomzal <wiktor.domzal@wp.pl>
2021-11-25 10:37:19 +0100

    Dodanie get i set

--- src/pl/softronic/szkolenia/java/gielda/Gielda.java
+++ src/pl/softronic/szkolenia/java/gielda/Gielda.java
@@ -3,6 +3,23 @@ package pl.softronic.szkolenia.java.gielda;
 public class Gielda {
 
     public String nazwa = "";
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
     public String adres = "";
 
     public static void main(String[] args) {
